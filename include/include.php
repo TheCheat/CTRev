e43735105af4326800b50e6f61f062c6d19ce9d6
thecheat <thecheat@TheCheat.cyber-tm.ru>
2012-10-02 18:20:46 +0700

    Removing /b/ from include/include.php from cyberhype

--- include/include.php
+++ include/include.php
@@ -3,8 +3,6 @@
 define('INSITE', true);
 if (ini_get('register_globals'))
     die("Отключите параметр register_globals в php.ini");
-if (!defined('ADMIN_FILE') && file_exists("install") && !IN_DEVELOPMENT)
-    die("Удалите инсталляцию движка(файл install.php и папку install из корня).<br/><b><u>Доступ ТОЛЬКО в АЦ!</u></b>");
 if (!function_exists('mb_internal_encoding'))
     die("Пожалуйста, установите на ваш хост модуль PHP - mbstring для поддержки многоязычности в движке.");
 
