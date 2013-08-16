7682b99a4efd7a5e4681ac1e6285c6a6e92816f2
Cyber-Team <Cyber-Team@100.66.0.11>
2013-08-16 19:14:31 +0700

    Forbidding to override standart string functions.

--- include/functions.php
+++ include/functions.php
@@ -623,9 +623,9 @@ function ok($print = false) {
  */
 function utf8_preg_offset($string, $offset, $backwards = false) {
     if ($backwards)
-        return mb_strlen(mb_substr($string, 0, $offset), "ISO-8859-1");
+        return strlen(mb_substr($string, 0, $offset));
     else
-        return mb_strlen(mb_substr($string, 0, $offset, "ISO-8859-1"));
+        return mb_strlen(substr($string, 0, $offset));
 }
 
 if (!function_exists('class_alias')) {
--- include/system/php_config.php
+++ include/system/php_config.php
@@ -26,6 +26,7 @@ if (IN_DEVELOPMENT) {
 @ini_set('ignore_repeated_errors', '1');
 @ini_set('sybct.allow_persistent', '0');
 @ini_set('default_socket_timeout', DEFAULT_SOCKET_TIMEOUT);
+@ini_set('mbstring.func_overload', 0); // Не нужно.
 @ignore_user_abort(1);
 @set_time_limit(MAX_SCRIPT_EXECUTION_TIME);
 if (function_exists("set_magic_quotes_runtime"))
